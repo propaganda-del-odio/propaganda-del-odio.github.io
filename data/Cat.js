2dd5bb12f5166350a84b378c3041802ab9bf6a86
ogardiazabal <ogardiazabal@OGARDIAZABAL-PC.widefense.com>
2017-10-03 18:23:40 -0300

    first release

---
+++ data/Cat.js
@@ -0,0 +1,77 @@
+
+define(function () {
+
+    return [
+        //15-sep
+        {
+            title: "Juncker asegura que la UE respetará el resultado del referéndum",
+            img: "http://www.elnacional.cat/uploads/s1/11/35/23/1/636239879960429985_2_302x302.jpg",
+            url: "http://www.elnacional.cat/ca/politica/juncker-europa-suport-referendum_191460_102.html",
+            date: "2017-09-15"
+        },
+        //1-oct
+        {
+            title: "Me han roto uno por uno los dedos de la mano y me han tocado los pechos",
+            img: "https://vozlibre.com/wp-content/uploads/2017/10/herida.jpg",
+            url: "http://www.naciodigital.cat/noticia/139586/trencat/dits/ma/tocat/pits",
+            date: "2017-10-01"
+        },
+        {
+            title: "Un vídeo muestra cómo un policía da una patada a una votante en Barcelona",
+            img: "https://www.ecestaticos.com/imagestatic/clipping/402/4b8/4024b8c073a885fb05566a2a278b1e0b/imagen-sin-titulo.jpg",
+            url: "https://www.elconfidencial.com/espana/cataluna/2017-10-01/referendum-cataluna-mossos-patada-voladora_1453235/",
+            date: "2017-10-01"
+        },
+        {
+            title: "Referéndum Catalunya : Policía Nacional dispara desde el furgón",
+            img: "https://i1.ytimg.com/vi/4Kn8xstCnyI/hqdefault.jpg",
+            url: "https://www.youtube.com/watch?v=4Kn8xstCnyI",
+            date: "2017-10-01"
+        },
+        {
+            title: "Las imágenes falsas y los bulos sobre el referéndum que circulan por las redes sociales",
+            img: "https://pbs.twimg.com/media/DLDWRahXcAEFlx_.jpg",
+            url: "http://www.20minutos.es/noticia/3149677/0/imagenes-falsas-bulos-referendum-cataluna-1o/",
+            date: "2017-10-01"
+        },
+        {
+            title: "Llevar España ante el TEDH (Tribunal Europeo de los Derechos Humanos)",
+            img: "https://assets.change.org/photos/7/vb/bi/uFvbbiYKvwNrDgy-800x450-noPad.jpg?1487282491",
+            url: "https://www.change.org/p/anc-portar-l-estat-espanyol-davant-el-tedh-tribunal-europeu-dels-drets-humans",
+            date: "2017-10-01"
+        },
+        //2-oct
+        {
+            title: "Presentador de Sky News corta a Dastis para decirle que \"Cataluña es más grande que 1 inquilino en 1 edificio\"",
+            img: "https://pbs.twimg.com/ext_tw_video_thumb/914845026525597698/pu/img/fJdhGNgPEUklBHQv.jpg",
+            url: "https://twitter.com/i/web/status/914845655507591168",
+            date: "2017-10-02"
+        },
+        {
+            title: "UGT y CCOO de Catalunya convocan huelga a pesar del rechazo de sus cúpulas estatales",
+            img: "http://www.elnacional.cat/uploads/s1/27/13/13/7/imatges_fotogaleria_referendum_1octubre09_2_302x302.jpg",
+            url: "http://www.elnacional.cat/es/politica/ugt-ccoo-huelga-general-referendum_197877_102.html",
+            date: "2017-10-02"
+        },
+        {
+            title: "El alto comisionado de la ONU pide una investigación de la violencia en Cataluña",
+            img: "http://www.ohchr.org/_layouts/15/OHCHR/OHCHR_images/logo_print.jpg",
+            url: "https://www.elconfidencial.com/mundo/2017-10-02/ato-comisionado-onu-investigacion-cataluna-referendum_1453719/",
+            date: "2017-10-02"
+        },
+        {
+            title: "Parlamento Europeo altera su orden del día para debatir la violencia policial en Cataluña",
+            img: "https://elcomunista.files.wordpress.com/2017/10/parlamento-europeo.jpg",
+            url: "https://elcomunista.net/2017/10/02/parlamento-europeo-altera-su-orden-del-dia-para-debatir-la-violencia-policial-en-cataluna/",
+            date: "2017-10-02"
+        },
+        //3-oct
+        {
+            title: "La fiscalía dice que las cargas del 1-O \"no afectaron en absoluto la convivencia\"",
+            img: "http://statics.ccma.cat/multimedia/jpg/4/9/1506970117394.jpg",
+            url: "http://www.ccma.cat/324/la-fiscalia-diu-que-les-carregues-de-l1-o-no-van-afectar-en-absolut-la-convivencia/noticia/2812662/",
+            date: "2017-10-03"
+        }
+    ];
+
+});
---
+++ data/Esp.js
@@ -0,0 +1,77 @@
+
+define(function () {
+
+    return [
+        //15-sep
+        {
+            title: "La respuesta del portavoz de la Comisión Europea sobre el referéndum catalán y las declaraciones del Presidente Juncker",
+            img: "https://pbs.twimg.com/amplify_video_thumb/908685079920201729/img/AdUOauw47QYm5gPu.jpg",
+            url: "https://twitter.com/prensace/status/908707446297948160",
+            date: "2017-09-15"
+        },
+        //1-oct
+        {
+            title: "Tumban a un policía con una silla cuando accedía a un colegio del referéndum",
+            img: "http://static1.laverdad.es/www/multimedia/201710/01/media/cortadas/silla-kIiD-U40922716463TtG-624x385@La%20Verdad.jpg",
+            url: "http://www.laverdad.es/nacional/tumban-policia-silla-20171001185133-nt.html",
+            date: "2017-10-01"
+        },
+        {
+            title: "Esta mujer votó 4 veces!!! En el referendum de Cataluña del 1-O",
+            img: "https://i1.ytimg.com/vi/n_OvGU3aJY0/hqdefault.jpg",
+            url: "https://www.youtube.com/watch?v=n_OvGU3aJY0",
+            date: "2017-10-01"
+        },
+        {
+            title: "Sangre falsa y más manipulaciones",
+            img: "https://somatemps.files.wordpress.com/2017/10/sangre-falsa.jpg?w=635",
+            url: "https://somatemps.me/2017/10/01/sangre-falsa-y-mas-manipulaciones/",
+            date: "2017-10-01"
+        },
+        {
+            title: "Interior eleva a 33 los policías y guardias civiles heridos al intervenir las urnas",
+            img: "https://okdiario.com/img/2017/10/01/barcelona-referendum-policia-carga-1-655x368.jpg",
+            url: "https://okdiario.com/espana/cataluna/2017/10/01/interior-eleva-33-policias-guardias-civiles-heridos-intervenir-urnas-1374889",
+            date: "2017-10-01"
+        },
+        {
+            title: "Cómo votar hasta cuatro veces en diferentes centros electorales",
+            img: "https://pbs.twimg.com/media/DLEAXpcXcAALJRm.jpg",
+            url: "http://www.larazon.es/espana/como-votar-dos-veces-en-diferentes-centros-electorales-CM16382734",
+            date: "2017-10-01"
+        },
+        //2-oct
+        {
+            title: "El resultado anunciado por la Generalitat es una muestra más de la nula credibilidad del referéndum ilegal. El recuento suma 100'88%",
+            img: "https://pbs.twimg.com/media/DLHmdGFXkAIbCts.jpg",
+            url: "https://twitter.com/i/web/status/914766624028229632",
+            date: "2017-10-02"
+        },
+        {
+            title: "Lo nunca visto: gitanos defienden a la Guardia Civil tras romper urnas en Gerona",
+            img: "https://widget.adprotv.com/pictures/5969/2455969/on12za9ad4s_crop2sub1.jpg",
+            url: "https://www.elespanol.com/reportajes/20171002/251224880_0.html",
+            date: "2017-10-02"
+        },
+        {
+            title: "El Punt Avui difunde imágenes de violencia policial de 2013 como si fueran de ayer",
+            img: "https://www.dolcacatalunya.com/wp-content/uploads/Captura-de-pantalla-2017-10-02-a-las-0.14.54-300x241.png",
+            url: "https://www.dolcacatalunya.com/2017/10/punt-avui-difunde-imagenes-violencia-policial-2013-fueran-ayer/",
+            date: "2017-10-02"
+        },
+        //3-oct
+        {
+            title: "Desmontan la mentira y denuncia falsa de la herida de los \"dedos rotos\"",
+            img: "https://vozlibre.com/wp-content/uploads/2017/10/herida.jpg",
+            url: "https://vozlibre.com/actualidad/desmontan-la-mentira-denuncia-falsa-la-herida-los-dedos-rotos-4854/",
+            date: "2017-10-03"
+        },
+        {
+            title: "Un ciudadano ruso dándonos lecciones. Escuchen a este hombre cargado de razones",
+            img: "https://pbs.twimg.com/ext_tw_video_thumb/915123391908270080/pu/img/pujHadtAbEyMT0QA.jpg",
+            url: "https://twitter.com/ajcastillam/status/915123869731770368",
+            date: "2017-10-03"
+        }
+    ]
+
+});
